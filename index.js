cf50131bf07bb175934f3c0996e935fce8b8f423
eblessing-eng <eblessing@stjohnseagles.org>
2026-04-14 14:08:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/', async (req, res) => {
   const resp = await fetch('https://api.stripe.com/v1/terminal/connection_tokens', {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer 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',
+      'Authorization': 'Bearer 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',
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   });
