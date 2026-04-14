c586640e1c56c14f510cc42ad23d3e2a57d5ed29
eblessing-eng <eblessing@stjohnseagles.org>
2026-04-14 14:59:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
@@ -13,16 +17,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/', async (req, res) => {
+app.post('/token', async (req, res) => {
   const resp = await fetch('https://api.stripe.com/v1/terminal/connection_tokens', {
     method: 'POST',
     headers: {
-     'Authorization': 'Bearer ' + process.env.STRIPE_SECRET_KEY,
+      'Authorization': 'Bearer ' + process.env.STRIPE_SECRET_KEY,
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   });
-  const data = await resp.json();
-  res.json(data);
+  res.json(await resp.json());
+});
+
+app.post('/charge', async (req, res) => {
+  const { amount, description } = req.body;
+  const params = new URLSearchParams();
+  params.append('amount', amount);
+  params.append('currency', 'usd');
+  params.append('payment_method_types[]', 'card_present');
+  params.append('capture_method', 'automatic');
+  params.append('description', description);
+  const resp = await fetch('https://api.stripe.com/v1/payment_intents', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + process.env.STRIPE_SECRET_KEY,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: params.toString()
+  });
+  res.json(await resp.json());
 });
 
 app.listen(process.env.PORT || 3000);
